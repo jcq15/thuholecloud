c601aa9cbcaa1590f1d5ea0399580919c4851f6b
jcq15 <33871330+jcq15@users.noreply.github.com>
2020-06-22 21:45:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 u_getcmt = r'https://thuhole.com/services/thuhole/api.php?action=getcomment&pid='
 u_getone = r'https://thuhole.com/services/thuhole/api.php?action=getone&pid='
 
-for i in range(12,27):
+for i in range(27):
     with open('data.txt', 'a', encoding='utf-8') as f:
         for j in range(1,101):
             f.write(requests.get(u_getone + str(i*100+j)).content.decode()+'\n')
